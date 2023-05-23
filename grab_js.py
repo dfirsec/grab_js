e2865ae0686137b02cc004f8a8f8092fa4c77937
Mitch Haddadi <12067255+dfirsec@users.noreply.github.com>
2023-05-23 16:51:58 -0400

    Updated

--- grab_js.py
+++ grab_js.py
@@ -1,4 +1,5 @@
-"""Grab JavaScript Code Blocks"""
+"""Grab JavaScript Code Blocks."""
+
 import os
 import random
 import re
@@ -9,82 +10,55 @@ import jsbeautifier
 import requests
 from bs4 import BeautifulSoup
 
-__author__ = "DFIRSec (@pulsecode)"
-__version__ = "0.0.3"
-__license__ = "MIT"
-
 
 class TermColors:
     """Returns terminal color options."""
 
-    BLUE = "\033[94m"
-    CYAN = "\033[96m"
-    GREEN = "\033[92m"
-    WARNING = "\033[93m"
-    FAIL = "\033[91m"
-    BOLD = "\033[1m"
-    GRAY = "\033[90m"
-    UNDERLINE = "\033[4m"
-    RST = "\033[0m"
-    SEP = f"{GRAY}{('.' * 50)}{RST}"
+    blue = "\033[94m"
+    cyan = "\033[96m"
+    green = "\033[92m"
+    yellow = "\033[93m"
+    red = "\033[91m"
+    bold = "\033[1m"
+    gray = "\033[90m"
+    underline = "\033[4m"
+    reset = "\033[0m"
+    separator = f"{gray}{('.' * 50)}{reset}"
 
 
-TC = TermColors()
+class UserAgent:
+    """Returns a random user agent."""
 
-UA = [
-    # Chrome
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
-    # Firefox
-    "Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
-    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)",
-    "Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko",
-    "Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; rv:11.0) like Gecko",
-    "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko",
-    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)",
-    "Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko",
-    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; Trident/7.0; rv:11.0) like Gecko",
-    "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)",
-    "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)",
-    "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)",
-]
+    def __init__(self) -> None:
+        """Initialize the user agent list."""
+        with open("user_agents.txt") as f:
+            self.user_agents = f.readlines()
 
-HEADERS = {"User-Agent": random.choice(UA)}
+    def get_random_user_agent(self) -> dict:
+        """Return a random user agent."""
+        return {"User-Agent": random.choice(self.user_agents).strip()}
 
-if len(sys.argv) > 1:
-    URL = sys.argv[1]
-else:
-    sys.exit("Usage: python grab_js.py <URL>")
 
-try:
-    PARENT = Path(__file__).parent
-    EXAMINE = Path.joinpath(PARENT, "examine_js.txt")
-    EXTRACTED = Path.joinpath(PARENT, "extracted_js.txt")
+class FilePaths:
+    """File paths for output."""
 
-    RESP = requests.get(URL, headers=HEADERS, timeout=3).text
-    SOUP = BeautifulSoup(RESP, "lxml")
-    JSCODE = SOUP.find_all("script")
-    CODEBLOCKS = [str(x) for x in JSCODE]
+    root = Path(__file__).parent
+    examine = Path.joinpath(root, "examine_js.txt")
+    extracted = Path.joinpath(root, "extracted_js.txt")
 
-    REGEX_PATTERN = (
+
+class RegexPatterns:
+    """Regex patterns for extracting JS."""
+
+    regex_patterns = [
         r"(?!document\.createElement\((\"|')(script|style|img|link|meta)(\"|')\))"
         r"(eval\(\S+\)|document\.write\(.+\)|unescape\(.+\)|setcookie\(.+\)|getcookie\(\S+\)|chrw?\(\S+\)"
         r"|strreverse\(\S+\)|charcode|tostring\((\S+|)\)|document\.createElement\(\S+\)|window\.open\(\S+\)"
         r"|window\.parent|window\.frameElement|window\.document($|\S+)|window\.onload|"
         r"(?=iframe).+(visibility=\"false\")|(?=iframe).+(width=\"0\" height=\"0\" frameborder=\"0\")|"
         r"<iframe src=.+<\/iframe>|var\s[a-z0-9_]{25,}\s?=|(?!\\x00)\\x[0-9a-fA-F]{2}\b|(?!\\u0000|\\u0026|"
-        r"\\u2029|\\u2026|\\u2028|\\u003c|\\u003e)\\u[0-9a-fA-F]{4})"
-    )
+        r"\\u2029|\\u2026|\\u2028|\\u003c|\\u003e)\\u[0-9a-fA-F]{4})",
+    ]
 
     #    (?!document\.createElement\((\"|')(script|style|img|link|meta)(\"|')\))        # negative lookahead for certain strings
     #    eval\(\S+\)                                                                    # matches eval() function
@@ -109,46 +83,113 @@ try:
     #    (?!\\x00)\\x[0-9a-fA-F]{2}\b                                                   # matches hex encoding with a value other than \\x00
     #    (?!\\u0000|\\u0026|\\u2029|\\u2026|\\u2028|\\u003c|\\u003e)\\u[0-9a-fA-F]{4}   # matches unicode encoding with disallowed characters
 
-    # erase file contents
-    if EXAMINE.exists() or EXTRACTED.exists():
-        with open(EXAMINE, "w", encoding="utf-8"):
-            pass
-        with open(EXTRACTED, "w", encoding="utf-8"):
-            pass
+    pattern = "|".join(regex_patterns)
 
-    # jsbeautifier options -- https://github.com/beautify-web/js-beautify
-    OPTS = jsbeautifier.default_options()
-    OPTS.jslint_happy = True
-    OPTS.max_preserve_newlines = -1
 
-    for code in CODEBLOCKS:
-        RESULTS = jsbeautifier.beautify(code, OPTS)
+def get_args() -> str:
+    """Extract the URL argument from the command line."""
+    if len(sys.argv) > 1:
+        return sys.argv[1]
 
-        if re.findall(REGEX_PATTERN, code, re.IGNORECASE | re.VERBOSE):
-            with open(EXAMINE, "a", errors="ignore", newline="", encoding="utf-8") as f:
-                f.write(f"{RESULTS}\n")
+    sys.exit("Usage: python grab_js.py <URL>")
 
-        with open(EXTRACTED, "a", errors="ignore", newline="", encoding="utf-8") as f:
-            f.write(f"{RESULTS}\n")
 
-    print(TC.SEP)
-    if EXAMINE.exists() and os.path.getsize(EXAMINE) != 0:
-        print(f"[*] Scrutinize this JS: {TC.CYAN}{EXAMINE.parts[-1]}{TC.RST}")
-        with open(EXAMINE, encoding="utf-8") as f:
-            LINES = [line.strip() for line in f.readlines()]
-            for n, line in enumerate(LINES, start=1):
-                MATCHES = re.finditer(REGEX_PATTERN, line, re.IGNORECASE)
-                for match in MATCHES:
-                    print(f"    > Line {n}: {TC.WARNING}{match.group()}{TC.RST} (chars {match.start()}-{match.end()})")
+def make_request(url: str) -> str:
+    """Create a session and make the request to the provided URL."""
+    ua = UserAgent()  # create an instance of UserAgent
+    headers = ua.get_random_user_agent()  # get a random user agent
+    session = requests.Session()
+    session.headers.update(headers)
+
+    with requests.Session() as session:
+        session.headers.update(headers)
+        return session.get(url, timeout=5).text
+
+
+def get_code_blocks(resp: str) -> list:
+    """Extract the JavaScript blocks from the webpage response."""
+    soup = BeautifulSoup(resp, "lxml")
+    jscode = soup.find_all("script")
+    return [str(x) for x in jscode]
+
+
+def clear_files() -> None:
+    """Clear the contents of the output files."""
+    if FilePaths.examine.exists() or FilePaths.extracted.exists():
+        open(FilePaths.examine, "w", encoding="utf-8").close()
+        open(FilePaths.extracted, "w", encoding="utf-8").close()
+
+
+def setup_beautifier() -> str:
+    """Set up the jsbeautifier with desired options."""
+    options = jsbeautifier.default_options()
+    options.jslint_happy = True
+    options.max_preserve_newlines = -1
+    return options
+
+
+def write_results_to_file(codeblocks: list, beautify_options: str) -> None:
+    """Beautify the JavaScript code and write results to the output files."""
+    examine_results = []
+    extracted_results = []
+
+    for code in codeblocks:
+        result = jsbeautifier.beautify(code, beautify_options)
+
+        if re.findall(RegexPatterns.pattern, code, re.IGNORECASE | re.VERBOSE):
+            examine_results.append(result)
+
+        extracted_results.append(result)
+
+    with open(FilePaths.examine, "a", errors="ignore", newline="", encoding="utf-8") as f:
+        f.write("\n".join(examine_results))
+
+    with open(FilePaths.extracted, "a", errors="ignore", newline="", encoding="utf-8") as f:
+        f.write("\n".join(extracted_results))
+
+
+def print_results() -> None:
+    """Print results of the JavaScript examination."""
+    tc = TermColors()
+
+    print(tc.separator)
+    if FilePaths.examine.exists() and os.path.getsize(FilePaths.examine) != 0:
+        print(f"[*] Scrutinize this JS: {tc.cyan}{FilePaths.examine.parts[-1]}{tc.reset}")
+        with open(FilePaths.examine, encoding="utf-8") as f:
+            lines = [line.strip() for line in f.readlines()]
+            for n, line in enumerate(lines, start=1):
+                matches = re.finditer(RegexPatterns.pattern, line, re.IGNORECASE)
+                for match in matches:
+                    print(f"    > Line {n}: {tc.yellow}{match.group()}{tc.reset} (chars {match.start()}-{match.end()})")
     else:
         print("[-] Hmm, nothing to scrutinize")
 
-    if EXTRACTED.exists() and os.path.getsize(EXTRACTED) != 0:
-        print(f"[~] All JS extracted: {TC.CYAN}{EXTRACTED.parts[-1]}{TC.RST}")
-    print(TC.SEP)
+    if FilePaths.extracted.exists() and os.path.getsize(FilePaths.extracted) != 0:
+        print(f"[~] All JS extracted: {tc.cyan}{FilePaths.extracted.parts[-1]}{tc.reset}")
+    print(tc.separator)
 
-except (requests.exceptions.MissingSchema, requests.exceptions.InvalidSchema) as err:
-    print(err)
-    sys.exit()
-except ConnectionError as err:
-    sys.exit(f"{TC.FAIL}[ERROR]{TC.RST} Please check the URL: {URL}")
+
+def url_processor(url: str) -> None:
+    """Process the URL and print the results."""
+    resp = make_request(url)
+    codeblocks = get_code_blocks(resp)
+    clear_files()
+    beautify_options = setup_beautifier()
+    write_results_to_file(codeblocks, beautify_options)
+    print_results()
+
+
+def main() -> None:
+    """Main function."""
+    url = get_args()
+    try:
+        url_processor(url)
+    except (requests.exceptions.MissingSchema, requests.exceptions.InvalidSchema) as err:
+        print(err)
+        sys.exit()
+    except ConnectionError:
+        sys.exit(f"{TermColors.red}[ERROR]{TermColors.reset} Please check the URL: {url}")
+
+
+if __name__ == "__main__":
+    main()
